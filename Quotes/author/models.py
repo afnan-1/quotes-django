d1263a40cc89ec7fa3f0314d47a27136f9263d22
afnan nadeem <roman.nadeem@gmail.com>
2020-12-06 23:32:35 +0500

    quotes filter author filter middle name full name okay

--- Quotes/author/models.py
+++ Quotes/author/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     first_name = models.CharField(max_length=20, blank=True, null=True)
+    middle_name = models.CharField(max_length=50, blank=True, null=True)
     last_name = models.CharField(max_length=20,blank=True, null=True)
     alias = models.CharField(max_length=1000,blank=True, null=True)
     photo = models.ImageField(upload_to='images/',blank=True, null=True)
@@ -28,6 +29,17 @@ class Author(models.Model):
     bio = models.TextField(max_length=500,blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    full_name = models.CharField(max_length=100,null= True, blank=True)
+    def save( self, *args, **kw ):
+        if self.middle_name!=None:
+            self.full_name = f'{self.first_name} {self.middle_name} {self.last_name}'
+        else:
+            self.full_name = f'{self.first_name} {self.last_name}'
+        super( Author, self ).save( *args, **kw )
     def __str__(self):
-        return self.first_name + " "+ self.last_name
+        if self.middle_name!=None:
+            return f'{self.first_name} {self.middle_name} {self.last_name}'
+        else:
+            return f'{self.first_name} {self.last_name}'
+
+    
--- Quotes/author/views.py
+++ Quotes/author/views.py
@@ -22,31 +22,27 @@ def home(request):
             print('hy')
             importted_data = dataset.load(new_author,format='xlsx')
             for data in importted_data:
-                dob = data[4]
-                dod = data[5]
-                attribute = data[7].capitalize()
-                value = Author(first_name=data[0],last_name=data[1],alias=data[2],age=data[3],date_of_birth=dob,date_of_death=dod,sex=data[6],attribute=attribute,bio=data[8])
+                dob = data[5]
+                dod = data[6]
+                attribute = data[8].capitalize()
+                sex = data[7].upper()
+                value = Author(first_name=data[0], middle_name=data[1], last_name=data[2],alias=data[3],age=data[4],date_of_birth=dob,date_of_death=dod,sex=sex,attribute=attribute,bio=data[9])
                 value.save()
 
         if 'myquotes' in request.FILES:
             quote_resource = QuoteResource
             dataset = Dataset()
             new_Quote = request.FILES['myquotes']
-            print('helo')
             if not new_Quote.name.endswith('xlsx'):
-                messages.info(request,'wrong format')
-                print('helo0')
+                messages.info(request,'wrong format please upload in xlsx format')
                 return render(request,'index.html')
-            print('hy')
             importted_data = dataset.load(new_Quote,format='xlsx')
-            import pdb;pdb.set_trace()
             for data in importted_data:
                 print(data[2])
-                author = Author.objects.get(first_name=data[2].split(' ')[0], last_name=data[2].split(" ")[1])
-                # author = Author.objects.get(first_name="", last_name='nadeem')
+                author = Author.objects.get(full_name=data[2])
                 value = Quote(quote=data[0],difficulty=data[1])
                 value.save()
-                value = Quote.objects.get(quote=data[0])
+                value = Quote.objects.get(pk=value.pk)
                 value.author = author
                 value.save()
     return render(request,'index.html')
