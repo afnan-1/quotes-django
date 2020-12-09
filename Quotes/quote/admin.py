3661d87fe8103d979bad95079281a4c552432a32
afnan nadeem <roman.nadeem@gmail.com>
2020-12-09 16:23:46 +0500

    site title

--- Quotes/quote/admin.py
+++ Quotes/quote/admin.py
@@ -3,7 +3,7 @@ from .models import Quote,AuthorAdmin
 
 # Register your models here.
 admin.site.register(Quote,AuthorAdmin)
-admin.site.site_header = "The Quotes"
-admin.site.index_title = "Quotes Adminstration"
+admin.site.site_header = "QOTRT"
+admin.site.index_title = "QOTRT Adminstration"
 admin.site.site_title = "Quotes Adminstration"
 
